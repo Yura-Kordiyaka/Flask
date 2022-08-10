23f46769f63d179f23a68306c35baaa9cbded4f6
Yura162 <yurakordiyaka2004@gmail.com>
2022-08-10 15:36:22 +0300

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 import request
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -13,15 +13,71 @@ def read():
     return data
 
 
-@app.route('/')
+@app.route('/', methods=['Get'])
 def general():
     d = read()
     return d
 
 
+@app.route('/change', methods=['GET'])
+def change():
+    return render_template('change.html')
+
+
+@app.route('/after_change', methods=['post'])
+def after_change():
+    form = request.form
+    dictionary = {
+        "Email": form['email'],
+        "first_name": form['fname'],
+        "id": form['id'],
+        "last_name": form['lname']
+    }
+    d = read()
+    h = d[0]
+    i = 0
+    for k in d:
+        if str(k['id']) == form['id']:
+            h = k
+    for k in range(0, len(d)):
+        if h == d[k]:
+            i = k
+    d[i] = dictionary
+    a = list(d)
+    with open("database/user.json", "w") as outfile:
+        outfile.write(json.dumps(a))
+        outfile.close()
+    return redirect('/')
+
+
 @app.route('/add', methods=['GET'])
 def index():
     return render_template("index.html")
+
+
+@app.route('/choose', methods=['get'])
+def choose():
+    return render_template("delete.html")
+
+
+@app.route('/delete', methods=['post'])
+def delete():
+    form = request.form
+    d = read()
+    f = d[0]
+    i = 0
+    for k in d:
+        if str(k['id']) == form['id']:
+            f = k
+    for k in range(0, len(d)):
+        if d[k] == f:
+            i = k
+    a = list(d)
+    a.pop(i)
+    with open("database/user.json", "w") as outfile:
+        outfile.write(json.dumps(a))
+        outfile.close()
+    return a
 
 
 @app.route('/show', methods=['POST'])
